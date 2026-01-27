4c2b00204c423de14cfcac223dd26ae904aa20b6
m0830295514-boop <m.0830295514@gmail.com>
2026-01-27 11:27:54 +0700

    card1

---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+ 
+class Home extends StatefulWidget {
+  const Home({super.key});
+ 
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+ 
+class _HomeState extends State<Home> {
+ 
+  int _selectedIndex = 0;
+ 
+  @override
+  Widget build(BuildContext context) {
+ 
+    // 8
+    List<Widget> pages = <Widget>[
+      Card1(),
+      Container(color: Colors.green),
+      Container(color: Colors.blue)
+      ];
+ 
+    void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      }
+    );
+  }
+ 
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Fooderlich',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        body: pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: onItemTapped,
+          items:
+            const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard),
+                label: 'Card 1',
+                ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard),
+                label: 'Card 2',
+                ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard),
+                label: 'Card 3',
+                ),
+            ],
+        ),
+    );
+  }
+}
